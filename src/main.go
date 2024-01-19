f21e6f70c40271331616ef06035d4b8f54c011c8
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-01-19 15:04:28 +1000

    chore: begin apis

---
+++ src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// type User struct {
+//     ID           primitive.ObjectID `bson:"_id,omitempty"`
+//     Email        string             `bson:"email,omitempty"`
+//     PreferredName string            `bson:"preferredName,omitempty"`
+// }
+
+func main() {
+	fmt.Println("Hello, World!")
+}
