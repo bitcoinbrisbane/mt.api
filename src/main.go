e46f9127570d17164ea80d47679e48be31aef31f
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-01-20 11:28:13 +1000

    chore: begin mongo

--- src/main.go
+++ src/main.go
@@ -3,12 +3,25 @@ package main
 import (
 	// "fmt"
 	// "net/http"
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	// "errors"
 )
+
+type MensTableWorker struct {
+	client *mongo.Client
+}
+
+func NewMensTableWorker(client *mongo.Client) *MensTableWorker {
+	return &MensTableWorker{client: client}
+}
 
 type table struct {
 	ID          int					`json:"id"`
@@ -39,7 +52,28 @@ func getTables(c *gin.Context) {
 }
 
 func getMen(c *gin.Context) {
+
+	collection := ConnectDB()
+
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+    if err != nil {
+        return
+    }
+
+    defer cursor.Close(context.TODO())
+
 	c.IndentedJSON(http.StatusOK, men)
+}
+
+func ConnectDB() *mongo.Collection {
+    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+    client, err := mongo.Connect(context.TODO(), clientOptions)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    collection := client.Database("your_db_name").Collection("users")
+    return collection
 }
 
 func main() {
